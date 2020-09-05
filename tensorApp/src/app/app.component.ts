e1d4581fc2dded1105cbc6c110747a39c97b9af3
areeja132000 <areej.ali@mail.utoronto.ca>
2020-09-05 10:02:12 -0400

    Getting audio

--- tensorApp/src/app/app.component.ts
+++ tensorApp/src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as tf from '@tensorflow/tfjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'tensorApp';
+
+  //model: tf.Model;
+  //predictions: any;
+
+  getResult() {
+    var audio = document.getElementById("aud");
+    //this.loadModel();
+    document.getElementById("result").innerHTML = "New text!"; //Should replace "New text!" with result
+  }
+
+  // Load KERAS model
+  async loadModel() {
+    //this.model = await tf.loadModel('./assets/model.json');
+  }
+  
+  /* //Predict
+  async predict() {
+
+      // Make predications
+      const output = this.model.predict(audio) as any;
+
+      // Save predictions
+      this.predictions = ;
+
+  } */
+  
+
 }
